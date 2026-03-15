30c472149fe37600c2170b426eb11f28df46fbf7
Franciszek Krawczyk <franciszek2005@gmail.com>
2026-03-15 21:38:15 +0100

    add a weird print method for statistics helper

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,5 +5,18 @@ class StatisticsHelper
 {
     private int x;
     private int y;
+
+    public void printStatistics()
+    {
+        for (int i = 0; i < x; i++)
+        {
+            Console.WriteLine(".");
+            for (int k = 0; k < y; k++)
+            {
+                Console.WriteLine("/");
+            }
+            Console.WriteLine("\n");
+        }
+    }
 }
 
